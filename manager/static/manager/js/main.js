7ccee4490426e935be5c377376973c24cd7e3d34
misha023 <m1kelyangio@gmail.com>
2023-05-11 16:45:50 +0300

    updatefiles

--- manager/static/manager/js/main.js
+++ manager/static/manager/js/main.js
@@ -1,5 +1,36 @@
 /* Когда пользователь нажимает на кнопку,
 переключение между скрытием и отображением раскрывающегося содержимого */
+
+
+const left_bar = document.getElementById("left-bar")
+const side_bar = document.getElementById("side-bar")
+const content_block = document.getElementById("content-block")
+const open_btn = document.getElementById("openSide_bar")
+
+function openleft_bar() {
+  left_bar.classList.add('active')
+}
+function closeleft_bar() {
+  left_bar.classList.remove('active')
+}
+
+
+
+function openSide_bar() {
+  side_bar.classList.remove('active')
+  open_btn.classList.remove('active')
+  content_block.classList.remove('active-content-block')
+}
+function closeSide_bar() {
+  side_bar.classList.add('active')
+  open_btn.classList.add('active')
+  content_block.classList.add('active-content-block')
+}
+
+
+
+
+
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
